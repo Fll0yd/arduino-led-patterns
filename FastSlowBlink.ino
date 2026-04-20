94d1d12e7dffa43da1178774b7d948734f3d71ee
Kenneth Lloyd Boller <122646869+Fll0yd@users.noreply.github.com>
2026-04-20 18:40:04 -0500

    Refactor LED blinking to use patterns

--- FastSlowBlink.ino
+++ FastSlowBlink.ino
@@ -1,18 +1,46 @@
 const int LED_PIN = 13;
+
+struct BlinkPattern {
+    int onTime;
+    int offTime;
+    int repeat;
+};
+
+BlinkPattern patterns[] = {
+    {100, 100, 5},   // Fast
+    {500, 500, 5}    // Slow
+};
+
+int currentPattern = 0;
+int blinkCount = 0;
+bool ledState = false;
+
+unsigned long previousMillis = 0;
+int interval = 100;
+
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
+    pinMode(LED_PIN, OUTPUT);
 }
+
 void loop() {
-  for (int i = 0; i < 5; i++) {
-    digitalWrite(LED_PIN, HIGH);
-    delay(100);
-    digitalWrite(LED_PIN, LOW);
-    delay(100);
-  }
-  for (int i = 0; i < 5; i++) {
-    digitalWrite(LED_PIN, HIGH);
-    delay(500);
-    digitalWrite(LED_PIN, LOW);
-    delay(500);
-  }
+    unsigned long currentMillis = millis();
+
+    if (currentMillis - previousMillis >= interval) {
+        previousMillis = currentMillis;
+
+        ledState = !ledState;
+        digitalWrite(LED_PIN, ledState);
+
+        if (ledState == false) {
+            blinkCount++;
+
+            if (blinkCount >= patterns[currentPattern].repeat) {
+                blinkCount = 0;
+                currentPattern = (currentPattern + 1) % 2;
+            }
+        }
+
+        interval = ledState ? patterns[currentPattern].onTime
+                            : patterns[currentPattern].offTime;
+    }
 }
